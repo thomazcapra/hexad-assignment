f7e779f7d8c6b8350664cd83afb85cc7fdf80d31
Thomaz Capra <thomaz.capra@millenniumbcp.pt>
2020-03-12 19:23:56 +0000

    Updates

---
+++ precache-manifest.12087b64aafae69c0c6f26ecec30db0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef4656e2765eb249d3816ba1df16836",
+    "url": "/hexad-assignment/index.html"
+  },
+  {
+    "revision": "2654ad122d0481ad1074",
+    "url": "/hexad-assignment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c9266f33e0edc70df6c",
+    "url": "/hexad-assignment/static/js/2.c9babd0e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hexad-assignment/static/js/2.c9babd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2654ad122d0481ad1074",
+    "url": "/hexad-assignment/static/js/main.e25ec0b4.chunk.js"
+  },
+  {
+    "revision": "9c18696acec386ccb70b",
+    "url": "/hexad-assignment/static/js/runtime-main.e794c374.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hexad-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
