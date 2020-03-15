019b96cb650793bcf4dee717c0e0049f2cfa1b83
Thomaz Capra <thomaz.capra@millenniumbcp.pt>
2020-03-15 20:22:06 +0000

    Added game service unit test

--- src/service/__test__/GameService.test.tsx
+++ src/service/__test__/GameService.test.tsx
@@ -1,9 +1,26 @@
 import { default as GameService } from '../GameService';
+import { act } from 'react-test-renderer';
 
 describe('GameService', (): void => {
   it('Should have initial state', async (): Promise<void> => {
     const initialList = await GameService.gameList$.value;
     expect(initialList).toBeDefined();
     expect(initialList).toHaveLength(14);
+  });
+
+  it('Should dispatch a new game list when shuffle is called ', async (): Promise<
+    void
+  > => {
+    const spyOnNext = jest.spyOn(GameService.gameList$, 'next');
+
+    expect(spyOnNext).not.toHaveBeenCalled();
+
+    await act(
+      async (): Promise<void> => {
+        await GameService.shuffle();
+      }
+    );
+
+    expect(spyOnNext).toHaveBeenCalled();
   });
 });
